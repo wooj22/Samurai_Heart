af2905d7682fd151c855a4d984a3248f95f0fd77
woojeong <77192050+wooj22@users.noreply.github.com>
2025-05-07 06:03:09 +0900

    leveling

--- Samurai_Heart/Samurai_Heart/Boss.h
+++ Samurai_Heart/Samurai_Heart/Boss.h
@@ -51,9 +51,9 @@ private:
 	float mapWidth;
 
 	// [Boss stat]
-	int hp = 500;
-	int maxHp = 500;
-	int power = 25;
+	int hp = 1500;
+	int maxHp = 1500;
+	int power = 30;
 	float speed = 150.f;
 	float jumpPower = 1000.f;
 	float gravity = 700.f;
